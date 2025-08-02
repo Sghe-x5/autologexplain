453f71918a36749762cbc148006fadb6ae02c9ec
Kol1n13 <kolin.1312@yandex.ru>
2025-08-02 19:47:54 +0500

    TimePicker

--- frontend/src/pages/App/App.tsx
+++ frontend/src/pages/App/App.tsx
@@ -5,7 +5,7 @@ import React from "react";
 function App() {
   const [date, setDate] = React.useState<Date | undefined>(undefined);
   return (<>
-    <DatePicker date={date} setDate={setDate}></DatePicker>
+    <DatePicker ></DatePicker>
   </>);
 }
 
