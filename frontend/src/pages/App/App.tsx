c12e831c3fa262a36849bc70e3c0a3b76a9e6caa
Kol1n13 <kolin.1312@yandex.ru>
2025-08-03 18:22:22 +0500

    base ModalOpening Button

--- frontend/src/pages/App/App.tsx
+++ frontend/src/pages/App/App.tsx
@@ -3,28 +3,44 @@ import "./App.css";
 import { Separator } from "@/components/ui/separator";
 import { BotAnswer } from "@/widgets/LogExplainModal/components/BotAnswer";
 import { Bot } from "lucide-react";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 function App() {
+  const [isModalOpen, setModalOpen] = useState<boolean>(false);
   return (
-    <div className="p-5">
-      <div className="flex gap-3">
-        <div className="p-2.5 w-fit h-fit bg-gray-200 rounded-lg">
-          <Bot />
+    <>
+      {isModalOpen &&
+      <div className="p-5">
+        <div className="flex gap-3">
+          <div className="p-2.5 w-fit h-fit bg-gray-200 rounded-lg">
+            <Bot />
+          </div>
+          <div>
+            <h1 className="font-[600] text-[#020817] text-[18px]">
+              ИИ Ассистент
+            </h1>
+            <p className="text-[#64748b] text-[14px]">
+              Настройте параметры для интеллектуального анализа логов
+            </p>
+          </div>
         </div>
-        <div>
-          <h1 className="font-[600] text-[#020817] text-[18px]">
-            ИИ Ассистент
-          </h1>
-          <p className="text-[#64748b] text-[14px]">
-            Настройте параметры для интеллектуального анализа логов
-          </p>
-        </div>
+        <Separator className="mt-5 mb-12" />
+        <LogExplainUI />
+        <Separator className="my-8" />
+        <BotAnswer />
       </div>
-      <Separator className="mt-5 mb-12" />
-      <LogExplainUI />
-      <Separator className="my-8" />
-      <BotAnswer />
-    </div>
+      }
+      <Button onClick={() => setModalOpen(!isModalOpen)} className="inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg]:size-4 [&amp;_svg]:shrink-0 bg-primary text-primary-foreground hover:bg-primary/90 fixed bottom-6 right-6 h-16 w-16 rounded-full shadow-xl hover:shadow-2xl transition-all duration-300 z-40 bg-gradient-to-r from-blue-600 to-purple-600 hover:from-blue-700 hover:to-purple-700 border-2 border-white/20 hover:scale-110 cursor-pointer">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-bot h-8 w-8 text-white">
+              <path d="M12 8V4H8"></path>
+              <rect width="16" height="12" x="4" y="8" rx="2"></rect>
+              <path d="M2 14h2"></path><path d="M20 14h2"></path>
+              <path d="M15 13v2"></path><path d="M9 13v2"></path>
+          </svg>
+          <span className="sr-only">Открыть ИИ Ассистента</span>
+      </Button>
+    </>
   );
 }
 
