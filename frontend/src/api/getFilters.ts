c9bbcc845aadaa8756ce03a28875e1ce77b08e73
Kol1n13 <kolin.1312@yandex.ru>
2025-08-10 19:33:10 +0500

    getFilters api

---
+++ frontend/src/api/getFilters.ts
@@ -0,0 +1,47 @@
+import { FILTER_URL } from "@/consts/api.const";
+import { FILTERS_MOCK } from "@/mocks/filter.mock";
+
+export type FilterData = {
+  product: string;
+  services: {
+    service: string;
+    environments: string[];
+  }[];
+};
+
+function validateFilters(data: unknown): asserts data is FilterData[] {
+  if (!Array.isArray(data)) {
+    throw new Error("Filters must be an array");
+  }
+
+  data.forEach((item, i) => {
+    if (typeof item.product !== "string") {
+      throw new Error(`Invalid 'product' at index ${i}`);
+    }
+    if (!Array.isArray(item.services)) {
+      throw new Error(`Invalid 'services' at index ${i}`);
+    }
+
+    item.services.forEach((srv: { service: any; environments: any[]; }, j: any) => {
+      if (typeof srv.service !== "string") {
+        throw new Error(`Invalid 'service' at [${i}][${j}]`);
+      }
+      if (
+        !Array.isArray(srv.environments) ||
+        !srv.environments.every((env: any) => typeof env === "string")
+      ) {
+        throw new Error(`Invalid 'environments' at [${i}][${j}]`);
+      }
+    });
+  });
+}
+
+export const GetFilters = async (): Promise<FilterData[]> => {
+  const fetchedData = await fetch(FILTER_URL || "", { method: "GET" })
+    .then((res) => res.json())
+    .catch(() => FILTERS_MOCK);
+
+  validateFilters(fetchedData);
+
+  return fetchedData;
+};
