adeab482a0579b866b7f66a93dd6524893a41c06
Kol1n13 <kolin.1312@yandex.ru>
2025-08-10 18:18:56 +0500

    small-style-fix

--- frontend/src/pages/App/App.tsx
+++ frontend/src/pages/App/App.tsx
@@ -5,20 +5,37 @@ import { createPortal } from "react-dom";
 import { useSelector } from "react-redux";
 import { LogExplainBtn } from "@/widgets/LogExplainModal/components/LogExplainBtn";
 import type { RootState } from "@/lib/store";
-import clsx from "clsx";
+import { useEffect } from "react";
 
 function App() {
   const modalRoot = document.getElementById("logExplainModal");
 
   const isShown = useSelector((state: RootState) => state.showModal.isShown);
 
+  useEffect(() => {
+    if (isShown) {
+      modalRoot?.classList.remove('translate-x-full', 'opacity-0');
+      modalRoot?.classList.add('translate-x-0', 'opacity-100');
+    } else {
+      modalRoot?.classList.remove('translate-x-0', 'opacity-100');
+      modalRoot?.classList.add('translate-x-full', 'opacity-0');
+    }
+  }, [isShown]);
+
   return (
     <main>
+      {isShown &&
+      <div
+          className="fixed inset-0 bg-[#18181B99] backdrop-blur-sm z-40"
+        >
+      </div>
+      }
       <img src="/images/AppBG.webp" />
       {modalRoot !== null &&
         isShown &&
         createPortal(<LogExplainUI />, modalRoot)}
       <LogExplainBtn />
+
     </main>
   );
 }
--- frontend/src/widgets/LogExplainModal/ui/LogExplainModalUI.tsx
+++ frontend/src/widgets/LogExplainModal/ui/LogExplainModalUI.tsx
@@ -10,7 +10,6 @@ import { Button } from "@/components/ui/button";
 
 export const LogExplainUI = () => {
   const dispatch = useDispatch<AppDispatch>();
-
   const hasLog = useLogStore((state) => state.log) !== null;
 
   const resetLog = useLogStore((state) => state.reset);
@@ -20,49 +19,49 @@ export const LogExplainUI = () => {
   };
 
   return (
-    <div className="w-full h-full p-5 absolute top-0 z-50 bg-white">
-      <div className="flex justify-between">
-        <div className="flex gap-3 items-center">
-          <div className="p-4 w-fit bg-[#DBE9FE] rounded-lg">
-            <Bot color="#2463EB" />
+      <div className="w-full h-full p-5 absolute top-0 z-50 bg-white">
+        <div className="flex justify-between">
+          <div className="flex gap-3 items-center">
+            <div className="p-4 w-fit bg-[#DBE9FE] rounded-lg">
+              <Bot color="#2463EB" />
+            </div>
+            <div>
+              <h1 className="font-[700] text-[#020817] text-[24px]">
+                AI Ассистент
+              </h1>
+              <p className="text-[#64748b] font-[400] text-[16px]">
+                {hasLog
+                  ? "Результаты анализа логов по заданным параметрам"
+                  : "Задайте параметры для анализа логов"}
+              </p>
+            </div>
           </div>
-          <div>
-            <h1 className="font-[700] text-[#020817] text-[24px]">
-              AI Ассистент
-            </h1>
-            <p className="text-[#64748b] font-[400] text-[16px]">
-              {hasLog
-                ? "Результаты анализа логов по заданным параметрам"
-                : "Задайте параметры для анализа логов"}
-            </p>
-          </div>
+          <button
+            className="p-4 w-fit h-fit bg-none"
+            onClick={() => dispatch(close())}
+          >
+            <X className="w-4 h-4" />
+          </button>
         </div>
-        <button
-          className="p-4 w-fit h-fit bg-none"
-          onClick={() => dispatch(close())}
-        >
-          <X className="w-4 h-4" />
-        </button>
+
+        {hasLog && (
+          <Button
+            type="button"
+            className="w-full mt-4 h-11 border"
+            variant="ghost"
+            onClick={onReset}
+          >
+            <RotateCcw /> Сбросить и задать новые параметры
+          </Button>
+        )}
+
+        <Separator className="my-5" />
+        {!hasLog && (
+          <section>
+            <LogExplainForm />
+          </section>
+        )}
+        {hasLog && <ChatWithAI />}
       </div>
-
-      {hasLog && (
-        <Button
-          type="button"
-          className="w-full mt-4 h-11 border"
-          variant="ghost"
-          onClick={onReset}
-        >
-          <RotateCcw /> Сбросить и задать новые параметры
-        </Button>
-      )}
-
-      <Separator className="my-5" />
-      {!hasLog && (
-        <section>
-          <LogExplainForm />
-        </section>
-      )}
-      {hasLog && <ChatWithAI />}
-    </div>
   );
 };
