ec2b65f5b59a7a817269a502467b4df62fccfc4c
Kol1n13 <kolin.1312@yandex.ru>
2025-08-10 15:53:46 +0500

    filters-loaded (mocks)

---
+++ frontend/src/mocks/filter.mock.ts
@@ -0,0 +1,28 @@
+export const FILTERS = [
+  {
+    "product": "yandex-go",
+    "services": [
+      {
+        "service": "taxi-api",
+        "environments": ["prod", "qa"]
+      },
+      {
+        "service": "maps-api",
+        "environments": ["prod", "preprod", "qa"]
+      }
+    ]
+  },
+  {
+    "product": "yandex-market",
+    "services": [
+      {
+        "service": "catalog-service",
+        "environments": ["prod", "dev"]
+      },
+      {
+        "service": "order-processing",
+        "environments": ["qa"]
+      }
+    ]
+  }
+]
