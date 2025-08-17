82ee998ebdfcfe3b407c8767194c2b88fbb58c87
Vlashex <rmaynavine@gmail.com>
2025-08-17 18:34:08 +0300

    fix crypto

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,16 @@ import { Provider } from "react-redux";
 import App from "./pages/App/App.tsx";
 import { store } from "./lib/store.ts";
 
+if (typeof crypto.randomUUID !== "function") {
+  (crypto as any).randomUUID = function polyfillUUID() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+      const r = crypto.getRandomValues(new Uint8Array(1))[0] & 15;
+      const v = c === "x" ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    });
+  };
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
