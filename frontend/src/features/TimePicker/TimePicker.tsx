646077c6f4b4b632b59f830999a160dcc19245e1
Kol1n13 <kolin.1312@yandex.ru>
2026-01-04 15:00:54 +0300

    TimeChoose

--- frontend/src/features/TimePicker/TimePicker.tsx
+++ frontend/src/features/TimePicker/TimePicker.tsx
@@ -1,48 +1,53 @@
-import { useState, useRef, useEffect } from 'react';
+'use client'
+
+import { useEffect, useState } from 'react'
 
 interface TimePickerProps {
-  label?: string;
-  value?: { hours: number; minutes: number };
-  onChange?: (time: { hours: number; minutes: number }) => void;
+  label?: string
+  value?: { hours: number; minutes: number }
+  onChange?: (time: { hours: number; minutes: number }) => void
 }
 
 const TimePicker = ({ label, value, onChange }: TimePickerProps) => {
   const [time, setTime] = useState({
     hours: value?.hours ?? 0,
-    minutes: value?.minutes ?? 0
-  });
-  
-  const hoursRef = useRef<HTMLDivElement>(null);
-  const minutesRef = useRef<HTMLDivElement>(null);
-  const [isOpen, setIsOpen] = useState(false);
+    minutes: value?.minutes ?? 0,
+  })
 
+  useEffect(() => {
+    if (onChange) {
+      onChange(time)
+    }
+  }, [time])
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const [h, m] = e.target.value.split(':').map(Number)
+    setTime({ hours: h, minutes: m })
+  }
 
   return (
-    <form className="max-w-[16rem] mx-auto grid grid-cols-2 gap-4">
-        <div>
-            <label htmlFor="start-time" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start time:</label>
-            <div className="relative">
-                <div className="absolute inset-y-0 end-0 top-0 flex items-center pe-3.5 pointer-events-none">
-                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                        <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z" clip-rule="evenodd"/>
-                    </svg>
-                </div>
-                <input type="time" id="start-time" className="bg-gray-50 border leading-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" min="09:00" max="18:00" value="00:00" required />
-            </div>
-        </div>
-        <div>
-            <label htmlFor="end-time" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">End time:</label>
-            <div className="relative">
-                <div className="absolute inset-y-0 end-0 top-0 flex items-center pe-3.5 pointer-events-none">
-                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                        <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z" clip-rule="evenodd"/>
-                    </svg>
-                </div>
-                <input type="time" id="end-time" className="bg-gray-50 border leading-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" min="09:00" max="18:00" value="00:00" required />
-            </div>
-        </div>
-    </form>
-  );
-};
+    <>
+      {label && (
+        <label
+          htmlFor="start-time"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          {label}
+        </label>
+      )}
+      <input
+        type="time"
+        id="start-time"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        value={`${time.hours.toString().padStart(2, '0')}:${time.minutes
+          .toString()
+          .padStart(2, '0')}`}
+        onChange={handleChange}
+        min="00:00"
+        max="23:59"
+      />
+    </>
+  )
+}
 
-export default TimePicker;
+export default TimePicker
