11f8175502b3b3f9692fb97c5b3c33168389ead6
Vlashex <rmaynavine@gmail.com>
2026-01-04 15:00:54 +0300

    add showModalSlice tests

---
+++ frontend/src/widgets/LogExplainModal/model/showModalSlice.test.ts
@@ -0,0 +1,22 @@
+import { describe, expect, it } from "vitest";
+import showModal, { close, open } from "./showModalSlice";
+
+describe("showModalSlice", () => {
+  it("should handle initial state", () => {
+    expect(showModal(undefined, { type: "unknown" })).toEqual({
+      isShown: false,
+    });
+  });
+
+  it("should handle open action", () => {
+    const initialState = { isShown: false };
+    const newState = showModal(initialState, open());
+    expect(newState.isShown).toBe(true);
+  });
+
+  it("should handle close action", () => {
+    const initialState = { isShown: true };
+    const newState = showModal(initialState, close());
+    expect(newState.isShown).toBe(false);
+  });
+});
